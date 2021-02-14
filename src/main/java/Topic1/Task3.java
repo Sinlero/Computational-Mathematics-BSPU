df8c2b2c1b4f199e1058c28cd6bc876d8a1d3fbb
Sinlero <wanted1345@gmail.com>
2021-02-14 12:21:00 +0900

    add Task3 solve

---
+++ src/main/java/Topic1/Task3.java
@@ -0,0 +1,16 @@
+package Topic1;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        double p = 2 * Math.sqrt(2);
+        for (int i = 2; i <= 60; i++) {
+            p = Math.pow(2, i) * Math.sqrt(2 * (1 - Math.sqrt(1 - Math.pow(p / Math.pow(2, i), 2))));
+            System.out.printf("n = %d Pn = %f  inaccuracy = %f\n", i, p, relativeInaccuracy(p, Math.PI));
+        }
+    }
+
+    public static double relativeInaccuracy(double algorithm, double embedded) {
+        return Math.abs(algorithm - embedded) / algorithm;
+    }
+}
