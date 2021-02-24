6923a8690dc737ebd9776e055133df2bc2265072
goldfinch_p8a <wanted1345@gmail.com>
2021-02-24 15:48:16 +0900

    add new algorithm

--- src/main/java/Topic1/Task3/Task3.java
+++ src/main/java/Topic1/Task3/Task3.java
@@ -4,8 +4,15 @@ public class Task3 {
 
     public static void main(String[] args) {
         double p = 2 * Math.sqrt(2);
+        System.out.println("Неустойчивый алгоритм");
         for (int i = 2; i <= 60; i++) {
             p = Math.pow(2, i) * Math.sqrt(2 * (1 - Math.sqrt(1 - Math.pow(p / Math.pow(2, i), 2))));
+            System.out.printf("n = %2s | Pn = %s | inaccuracy = %s\n", i, p, relativeInaccuracy(p, Math.PI));
+        }
+        System.out.println("Устойчивый алгоритм");
+        p = 2 * Math.sqrt(2);
+        for (int i = 2; i <= 60; i++) {
+            p = p * Math.sqrt(2 / (1 + Math.sqrt(1 - Math.pow(p / Math.pow(2, i), 2))));
             System.out.printf("n = %2s | Pn = %s | inaccuracy = %s\n", i, p, relativeInaccuracy(p, Math.PI));
         }
     }
