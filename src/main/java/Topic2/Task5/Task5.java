89c828312ea6a67680f2b8d83c4584f3934b8f29
goldfinch_p8a <wanted1345@gmail.com>
2021-03-04 11:14:56 +0900

    complete task

--- src/main/java/Topic2/Task5/Task5.java
+++ src/main/java/Topic2/Task5/Task5.java
@@ -1,23 +1,41 @@
 package Topic2.Task5;
 
+import Topic1.Task2.Task2;
+
 public class Task5 {
     public static void main(String[] args) {
-        sum(1_000_000_000);
+        int n1 = 10;
+        int n2 = 8;
+        for (int i = 1; i <= 8; i++) {
+            n1 *= 10;
+            sum(n1);
+        }
+        System.out.println();
+        for (int i = 1; i <= 8; i++) {
+            n2 *= 10;
+            secondSum(n2);
+        }
     }
 
     public static void sum(int n) {
         float value = 0.1F;
-        for (int i = 1; i <= n; i++) {
+        float exactValue = value * n;
+        int i;
+        for (i = 1; i < n; i++) {
             value += 0.1F;
-            System.out.printf("|%3s | %13s|\n", i, value);
         }
+        System.out.printf("|%10s | %13s| %10s | %13s |\n", i, value, exactValue,
+                                                         Task2.relativeInaccuracy(value, exactValue) * 100);
     }
 
     public static void secondSum(int n) {
         float value = 0.125F;
-        for (int i = 1; i <= n; i++) {
+        float exactValue = value * n;
+        int i;
+        for (i = 1; i < n; i++) {
             value += 0.125F;
-            System.out.printf("|%3s | %13s|\n", i, value);
         }
+        System.out.printf("|%10s | %13s| %10s | %13s |\n", i, value, exactValue,
+                                                         Task2.relativeInaccuracy(value, exactValue) * 100);
     }
 }
