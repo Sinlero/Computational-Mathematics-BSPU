4cb5e2050f69a0327cac698bc6e3e359cf7c1eae
goldfinch_p8a <wanted1345@gmail.com>
2021-03-18 14:42:28 +0900

    work in progress on getMaxNorm

--- src/main/java/Topic3/Task13/Task13.java
+++ src/main/java/Topic3/Task13/Task13.java
@@ -4,6 +4,9 @@ import org.apache.commons.math3.linear.MatrixUtils;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealMatrixFormat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task13 {
     public static void main(String[] args) {
         double[][] firstData = {
@@ -29,5 +32,22 @@ public class Task13 {
         System.out.printf("Результат вычитания матриц:\n%s\n", output.format(firstMatrix.subtract(secondMatrix)));
         System.out.printf("Третья матрица:\n%s\n", output.format(thirdMatrix));
         System.out.printf("Результат перемножения матриц:\n%s\n", output.format(firstMatrix.multiply(thirdMatrix)));
+        System.out.println("||.||1 = " + thirdMatrix.getNorm()); // ||.||1 по столбцам
+        getMaxNorm(secondMatrix);
+        getMaxNorm(thirdMatrix);
+    }
+
+    public static void getMaxNorm(RealMatrix matrix) {  //  ||.||infinity по строкам
+        List<Double> rowValues = new ArrayList<>();
+        Double sum = new Double(0);
+        double[][] data = matrix.getData();
+        for (int i = 0; i < data.length; i++) {
+            double[] row = matrix.getRow(i);
+            for (int j = 0; j < row.length; j++) {
+                sum += Math.abs(row[j]);
+            }
+            System.out.println(sum);
+            sum = 0.0;
+        }
     }
 }
